57b8732b2ae534dd0d8e8ef466b082dc163eabcd
fwang27 <35862295+fwang27@users.noreply.github.com>
2021-05-22 06:41:43 -0700

    Update UpdatePostInfo.js
    import .scss

--- MERN_new/frontendreact/src/components/UpdatePostInfo.js
+++ MERN_new/frontendreact/src/components/UpdatePostInfo.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import '../App.css';
+import '../App.scss';
 
 class UpdatePostInfo extends Component {
   constructor(props) {
