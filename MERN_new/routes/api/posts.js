0ec718da0260b22644e47b78c23c7e0b6911224b
skyli00 <83743338+skyli00@users.noreply.github.com>
2021-05-30 16:46:34 -0700

    Add files via upload

--- MERN_new/routes/api/posts.js
+++ MERN_new/routes/api/posts.js
@@ -58,4 +58,25 @@ router.delete('/:id', (req, res) => {
     .catch(err => res.status(404).json({ error: 'No such a Post' }));
 });
 
+router.post("/search", async (req, res) => {
+  try {
+    const query = req.body.query
+    const queryList = query.split(" ")
+
+    allQueries = []
+    queryList.forEach(element => {
+      allQueries.push({ title: { $regex: String(element) } })
+    });
+    const allPosts = await Post.find({ $or: allQueries })
+
+    if (!allPosts || allPosts.length === 0) {
+      res.status(400).json({ message: "No post was found" })
+    }
+
+    res.status(200).json(allPosts)
+  } catch (err) {
+    res.json({ message: err });
+  }
+})
+
 module.exports = router;
