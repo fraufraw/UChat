5255acb4a7492cd67f7fde0ff48aa42651baae44
william <williamwei7062@gmail.com>
2021-05-31 13:30:46 -0700

    regex search

--- MERN_new/routes/api/search.js
+++ MERN_new/routes/api/search.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const Post = require('../../models/Posts');
 
 router.post('/', (req, res) => {
-  Post.find({title: req.body.title})//{$exists: req.body.title}
+  Post.find({title: {$regex: req.body.title}})//{$exists: req.body.title}
     .then(posts => res.json(posts))
     .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
 });
