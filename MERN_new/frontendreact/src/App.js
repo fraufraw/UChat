b4bf8028f04badb3c39a0e91f63adbcb9ebedbfd
skyli00 <83743338+skyli00@users.noreply.github.com>
2021-06-01 15:45:51 -0700

    Finished Search

--- MERN_new/frontendreact/src/App.js
+++ MERN_new/frontendreact/src/App.js
@@ -8,6 +8,9 @@ import ShowPostDetails from './components/ShowPostDetails';
 import UpdatePostInfo from './components/UpdatePostInfo';
 import LoginRegister from './components/LoginRegister';
 import Searchtitle from './components/Searchtitle';
+import Searchcomment from './components/Searchcomment';
+import Searchdescription from './components/SearchbyDescription';
+import Search from './components/Search';
 import myposts from './components/myposts';
 
 class App extends Component {
@@ -21,6 +24,9 @@ class App extends Component {
           <Route path='/edit-Post/:id' component={UpdatePostInfo} />
           <Route path='/show-Post/:id' component={ShowPostDetails} />
           <Route path='/searchtitle' component={Searchtitle} />
+          <Route path='/searchcomment' component={Searchcomment} />
+          <Route path='/searchdescription' component={Searchdescription} />
+          <Route path='/search' component={Search} />
           <Route path='/myposts' component={myposts} />
         </div>
       </Router>
