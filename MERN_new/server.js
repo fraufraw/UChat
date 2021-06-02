31d1dea373e514d5b0ada1e0a44c2557dbb86a8e
Peifeng Xie <2900948406@qq.com>
2021-06-01 20:33:42 -0700

    Add 'othersposts' to server.js

--- MERN_new/server.js
+++ MERN_new/server.js
@@ -7,6 +7,7 @@ const message=require('./routes/api/message');
 const user=require('./routes/api/user');
 const searchtitle=require('./routes/api/searchtitle');
 const myposts=require('./routes/api/myposts');
+const othersposts=require('./routes/api/othersposts');
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use('/api/message', message);
 app.use('/api/user', user);
 app.use('/api/searchtitle', searchtitle);
 app.use('/api/myposts', myposts);
+app.use('/api/othersposts', othersposts);
 
 const port = process.env.PORT || 8082;
 
