e4b1a3d66ecd1e889f995145fac8e9019c8a2b35
Peifeng Xie <2900948406@qq.com>
2021-06-01 20:37:16 -0700

    OthersPosts.js used to see others' profiles in his posts

---
+++ MERN_new/frontendreact/src/components/OthersPosts.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import '../App.css';
+import axios from 'axios';
+import { Router,Route,hashHistory} from 'react-router';
+import { Link,BrowserRouter} from 'react-router-dom';
+import PostCard from './PostCard';
+
+class OthersPosts extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        posts: [],
+        keyword: '',
+
+        UserName: this.props.history.location.state.UserName, 
+        PassWord: this.props.history.location.state.PassWord,
+        userId: this.props.history.location.state.userId,
+        publisher: this.props.history.location.state.publisher
+
+      };
+    }
+
+    componentDidMount() {
+        const data = {
+          user: this.state.publisher,
+         };
+
+    axios
+      .post('http://localhost:8082/api/othersposts', data)
+      .then(res => {
+        this.setState({
+          keyword: '',
+          posts: res.data
+        })
+ 
+      })
+      .catch(err => {
+        console.log("Error in Searchtitle!");
+      })
+  };
+
+  render() {
+
+    const posts = this.state.posts;
+    console.log("PrintPost: " + posts);
+    let postList;
+
+    if(!posts) {
+      postList = "there is no post record!";
+    } else {
+      postList = posts.map((post, k) =>
+        <PostCard post={post} UserName={this.state.UserName} PassWord={this.state.PassWord} userId={this.state.userId} key={k} />
+      );//this is a way to past parameter to another function
+    }
+
+
+    return (
+      <div className="Searchtitle">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <br />
+              <Link to={{pathname:"/show-list", state:{UserName: this.state.UserName,
+                            PassWord: this.state.PassWord,
+                            userId: this.state.userId}}} className="btn btn-outline-warning float-right">
+                  Back
+              </Link>
+            </div>
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">{this.state.publisher}{"'s Profile"}</h1>
+              <h4> {this.state.publisher} {"'s Profile"}</h4>
+              <div className="list">
+                {postList}
+              </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default OthersPosts;
